850c18eca976d87feb71215abde9006ac35cffbc
Suraj Anand <suraj@Surajs-MacBook-Pro.local>
2020-04-27 16:24:50 +0545

    first commit

---
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -0,0 +1,84 @@
+/*@author 
+ * 
+ * Suraj*/
+
+
+
+package com.crm.qa.base;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+
+import com.crm.qa.util.TestUtil;
+import com.crm.qa.util.WebEventListener;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+//import test.OrangeHrmLoginPageTest_TestNG_Demo;
+
+public class TestBase {
+
+	public static WebDriver driver = null;
+	public static Properties prop = new Properties();
+	static String projectPath = System.getProperty("user.dir");
+	public static EventFiringWebDriver e_driver;
+	public static WebEventListener eventListener;
+
+	public TestBase() {
+
+		try {
+
+			InputStream input = new FileInputStream(projectPath+"/src/main/java/com/crm/qa/config/config.properties");
+			prop.load(input);
+			//String browser =  prop.getProperty("browser");
+			//System.out.println(browser);
+			
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			System.out.println(e.getCause());
+			e.printStackTrace();
+		}
+
+	}
+	
+	public static void initialization() {
+		String browserName = prop.getProperty("browser");
+		if (browserName.equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			
+		}
+		else if (browserName.equalsIgnoreCase("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		}
+		else if (browserName.equalsIgnoreCase("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+		
+		e_driver= new EventFiringWebDriver(driver);
+		// Now create object of EventListerHandler to register it with EventFiringWebDriver
+
+		eventListener = new WebEventListener();
+		e_driver.register(eventListener);
+		driver = e_driver;
+		
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
+		
+		driver.get(prop.getProperty("url"));
+	}
+
+}
