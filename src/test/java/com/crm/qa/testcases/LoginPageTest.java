de861f231477eb0921ac2fa3f8a85b041bef7367
Suraj Anand <Suraj@Surajs-MacBook-Pro.local>
2020-05-11 19:06:52 +0545

    changes

--- src/test/java/com/crm/qa/testcases/LoginPageTest.java
+++ src/test/java/com/crm/qa/testcases/LoginPageTest.java
@@ -11,63 +11,61 @@ import com.crm.qa.pages.LoginPage;
 
 public class LoginPageTest extends TestBase{
 
-	//	LoginPage loginPage;
-	//	HomePage homePage;
+		LoginPage loginPage;
+		HomePage homePage;
 
 	public LoginPageTest() {
 		super();
 	}
 
-	//	@BeforeMethod
-	//	public void setUp() {
-	//		initialization();
-	//		loginPage = new LoginPage();
-	//		
-	//		
-	//	}
+		@BeforeMethod
+		public void setUp() {
+			initialization();
+			loginPage = new LoginPage();
+			
+			
+		}
 
-	//	@Test(priority = 1)
-	//	public void LoginPageTitleTest() {
-	//		String title = loginPage.validateLoginPageTitle();
-	//		Assert.assertEquals(title, "CRMPRO - CRM software for customer relationship management, sales, and support.");
-	//
-	//
-	//	}
-	//	
-	//	@Test(priority = 2)
-	//	public void CRMLogoImageTest() {
-	//		boolean flag = loginPage.validateCrmImage();
-	//		Assert.assertTrue(flag);
-	//	}
+		@Test(priority = 1)
+		public void LoginPageTitleTest() {
+			String title = loginPage.validateLoginPageTitle();
+			Assert.assertEquals(title, "CRMPRO - CRM software for customer relationship management, sales, and support.");
+	
+	
+		}
+		
+		@Test(priority = 2)
+		public void CRMLogoImageTest() {
+			boolean flag = loginPage.validateCrmImage();
+			Assert.assertTrue(flag);
+		}
 
 	@Test(priority = 1)
 	public void LoginTest() {
 		log.info("****************************** starting test case *****************************************");
 		log.info("****************************** LoginTest *****************************************");
-		System.out.println("Test");
+		
 
-		System.out.println("\n Hello world ... \n");
-
-
-		log.trace("This is a trace message");
-		log.info("This is information message.");
-		log.error("This is an error message.");
-		log.warn("This is a warning message.");
-		log.fatal("This is a fatal message.");
+//
+//		log.trace("This is a trace message");
+//		log.info("This is information message.");
+//		log.error("This is an error message.");
+//		log.warn("This is a warning message.");
+//		log.fatal("This is a fatal message.");
 
 
 		System.out.println("\n  Completed successfully...");
-		//		homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+		homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
 
 		log.info("****************************** ending test case *****************************************");
 		log.info("****************************** LoginTest *****************************************");
 
 	}
 
-//	@AfterMethod
-//	public void tearDown() {
-//		driver.quit();
-//	}
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
 
 
 
